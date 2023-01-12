6cf89ba31110017236a6880357387830b2b78f8e
Yukihiro Chiba <chiba.yukihiro.biz@gmail.com>
2023-01-12 18:43:50 +0900

    refactor change to use ruby standard functions

--- quiz/math-pazzle/q2/q2.rb
+++ quiz/math-pazzle/q2/q2.rb
@@ -9,7 +9,7 @@
 
 module Q2
 
-  OPERATIONS  = ['+', '-', '*', '/', ' '].freeze
+  OPERATIONS  = ['+', '-', '*', '/', ''].freeze
 
   def self.main
     puts (1000..9999).filter {|n| reversible?(n)}
@@ -17,25 +17,22 @@ module Q2
 
   def self.reversible?(n)
     n_str = n.to_s
-    OPERATIONS.each {|o1|
-      OPERATIONS.each {|o2|
-        OPERATIONS.each {|o3|
-          next if "#{o1}#{o2}#{o3}".eql?('   ')
-          n_expr = n_str.dup.insert(1, o1).insert(3, o2).insert(5, o3).gsub(' ', '')
-          # 不要な 0 を除去する
-          # ruby の eval は 0 から始まる値を8進数として処理するため、そのままでは四則演算でエラーが発生する
-          # ex) eval(15+08) => invalid octal digit error
-          n_expr.gsub!(/(\D)0+(\d)/, '\1\2')
-        begin
-          r_result = eval(n_expr).to_s.reverse
-          return true if r_result.eql? n_str
-        rescue ZeroDivisionError => e
-          next
-        end
-        }
-      }
-    }
+    format = n_str.split('').join('%s') # 1234 -> 1%s2%s3%s4
 
+    OPERATIONS.repeated_permutation(3) {|o1, o2, o3|
+    begin
+      next if [o1, o2, o3].join.empty?
+      n_expr = sprintf(format, o1, o2, o3)
+      # 不要な 0 を除去する
+      # ruby の eval は 0 から始まる値を8進数として処理するため、そのままでは四則演算でエラーが発生する
+      # ex) eval(15+08) => invalid octal digit error
+      n_expr.gsub!(/(\D)0+(\d)/, '\1\2')
+      r_result = eval(n_expr).to_s.reverse
+      return true if r_result.eql? n_str
+    rescue ZeroDivisionError => e
+      next
+    end
+    }
     return false
   end
 end
